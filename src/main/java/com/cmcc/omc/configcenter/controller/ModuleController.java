f84aa59db96f8ecd85fab23f896a5ea6284eda06
hejiahuan <hejiahuan@cmhi.chinamobile.com>
2018-01-09 20:21:46 +0800

    aa

--- src/main/java/com/cmcc/omc/configcenter/controller/ModuleController.java
+++ src/main/java/com/cmcc/omc/configcenter/controller/ModuleController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -72,6 +73,12 @@ public class ModuleController {
 		model.addAttribute("id", m.getId());
 		model.addAttribute("name", m.getName());
 		model.addAttribute("updateName", m.getUpdateName());
-		return "modiPage";
+		return "moduleModify";
+	}
+	
+	@RequestMapping("/update")
+	public String update(Module m,RedirectAttributes red){
+		
+		return "redirect:/module/index";
 	}
 }
